ac203e292b6b56667aea517cacad621b84910f9b
Artem Klochkov <Arthorror@gmail.com>
2019-08-06 00:55:19 +0300

    remove unused reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux';
 import { authentication } from './authentication';
-// import { notification } from './notification';
 
-// const rootReducer = combineReducers({authentication, notification});
 const rootReducer = combineReducers({authentication});
 export default rootReducer;
